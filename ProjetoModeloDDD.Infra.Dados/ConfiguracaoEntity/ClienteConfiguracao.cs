929495ac643196cf7fda1df457852a285012dc08
Rodrigo Marcell <rodrigomarcell@outlook.com>
2021-10-21 13:02:37 -0400

    Criando ProdutoConfiguracao e ClienteConfiguracao

---
+++ ProjetoModeloDDD.Infra.Dados/ConfiguracaoEntity/ClienteConfiguracao.cs
@@ -0,0 +1,26 @@
+﻿using ProjetoModeloDDD.Dominio.Entidades;
+using System.Data.Entity.ModelConfiguration;
+
+namespace ProjetoModeloDDD.Infra.Dados.ConfiguracaoEntity
+{
+    public class ClienteConfiguracao : EntityTypeConfiguration<Cliente>
+    {
+        public ClienteConfiguracao()
+        {
+            HasKey(c => c.ID);
+
+            Property(c => c.Nome)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            Property(c => c.SobreNome)
+                .IsRequired()
+                .HasMaxLength(150);
+
+
+            Property(c => c.Email)
+                .IsRequired();              
+        }
+
+    }
+}
---
+++ ProjetoModeloDDD.Infra.Dados/ConfiguracaoEntity/ProdutoConfiguracao.cs
@@ -0,0 +1,31 @@
+﻿using ProjetoModeloDDD.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoModeloDDD.Infra.Dados.ConfiguracaoEntity
+{
+    class ProdutoConfiguracao : EntityTypeConfiguration<Produto>
+    {
+        public ProdutoConfiguracao()
+        {
+            HasKey(p => p.ID);
+
+
+            Property(p => p.Nome)
+                .IsRequired()
+                .HasMaxLength(250);
+
+            Property(p => p.Valor)
+                .IsRequired();
+
+            HasRequired(p => p.Cliente)
+                .WithMany()
+                .HasForeignKey(p => p.ClientId);
+
+        }
+    }
+}
