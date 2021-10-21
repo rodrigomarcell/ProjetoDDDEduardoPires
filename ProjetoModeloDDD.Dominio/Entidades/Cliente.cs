0829f181444099b5b8e5e1e6a64d042bd0189de3
Rodrigo Marcell <rodrigomarcell@outlook.com>
2021-10-21 12:00:33 -0400

    Add project files.

---
+++ ProjetoModeloDDD.Dominio/Entidades/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ProjetoModeloDDD.Dominio.Entidades
+{
+    public class Cliente
+    {
+        public int ID { get; set; }
+
+        public string Nome { get; set; }
+
+        public string SobreNome { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+        public bool Ativo { get; set; }
+
+
+        public bool ClienteEspecial(Cliente cliente)
+        {
+            return cliente.Ativo && DateTime.Now.Year - cliente.DataCadastro.Year >= 5;
+        }
+    }
+}
---
+++ ProjetoModeloDDD.Infra.Dados/Contexto/Contexto.cs
@@ -0,0 +1,58 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using ProjetoModeloDDD.Dominio.Entidades;
+using System.Linq;
+using System;
+
+namespace ProjetoModeloDDD.Infra.Dados.Contexto
+{
+    public class Contexto : DbContext
+    {
+        public Contexto() : base("DBDDDEduardoPires")
+        {
+
+        }
+
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {        
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            modelBuilder.Properties()
+                .Where(p => p.Name == p.ReflectedType.Name + "Id")
+                .Configure(p => p.IsKey());
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasColumnType("varchar"));
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasMaxLength(100));
+        }
+
+        public override int SaveChanges()
+        {
+
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                if(entry.State == EntityState.Added)
+                {
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                }
+
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("DataCadastro").IsModified = false;
+                }
+
+            }
+                        
+            return base.SaveChanges();
+        }
+
+    }
+}
