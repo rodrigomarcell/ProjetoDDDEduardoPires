893ce046fa62ea5c403e51c48e1b470216dad0be
Rodrigo Marcell <rodrigomarcell@outlook.com>
2021-10-22 11:49:18 -0400

    criando view models Cliente e Produto

---
+++ ProjetoModeloDDD.MVC/ViewModels/ClienteViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoModeloDDD.MVC.ViewModels
+{
+    public class ClienteViewModel
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo Nome")]
+        [MaxLength(150, ErrorMessage = "Máximo de {0} caracteres")]
+        [MinLength(2, ErrorMessage = "Mínimo de {0} caracteres")]
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo Sobrenome")]
+        [MaxLength(150, ErrorMessage = "Máximo de {0} caracteres")]
+        [MinLength(2, ErrorMessage = "Mínimo de {0} caracteres")]
+        [Display(Name = "Sobrenome")]
+        public string SobreNome { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo email")]
+        [MaxLength(100, ErrorMessage = "Máximo de {0} caracteres")]
+        [EmailAddress(ErrorMessage = "Preencha um email válido.")]
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+
+        [ScaffoldColumn(false)]
+        public DateTime DataCadastro { get; set; }
+
+        public bool Ativo { get; set; }
+
+        public virtual IEnumerable<ProdutoViewModel> Produtos { get; set; }
+    }
+}
---
+++ ProjetoModeloDDD.MVC/ViewModels/ProdutoViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoModeloDDD.MVC.ViewModels
+{
+    public class ProdutoViewModel
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo Nome")]
+        [MaxLength(150, ErrorMessage = "Máximo de {0} caracteres")]
+        [MinLength(2, ErrorMessage = "Mínimo de {0} caracteres")]
+        public string Nome { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Range(typeof(decimal),"0","999999999")]
+        [Required(ErrorMessage = "Preencha um valor")]
+        public decimal Valor { get; set; }
+
+        [DisplayName("Disponível?")]
+        public bool Disponivel { get; set; }
+
+        public int ClientId { get; set; }
+
+        public virtual ClienteViewModel Cliente { get; set; }
+
+    }
+}
