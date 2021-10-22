c82a61a7416b10c96fe98a284faf88d51e523472
Rodrigo Marcell <rodrigomarcell@outlook.com>
2021-10-22 11:46:57 -0400

    Criando repositorios Cliente e Produtos

---
+++ ProjetoModeloDDD.Infra.Dados/Repositorios/ClienteRepositorio.cs
@@ -0,0 +1,10 @@
+﻿using ProjetoModeloDDD.Dominio.Entidades;
+using ProjetoModeloDDD.Dominio.Interfaces;
+
+namespace ProjetoModeloDDD.Infra.Dados.Repositorios
+{
+    public class ClienteRepositorio : RepositorioBase<Cliente>, IRepositorioCliente
+    {
+
+    }
+}
---
+++ ProjetoModeloDDD.Infra.Dados/Repositorios/ProdutoRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using ProjetoModeloDDD.Dominio.Entidades;
+using ProjetoModeloDDD.Dominio.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoModeloDDD.Infra.Dados.Repositorios
+{
+    public class ProdutoRepositorio : RepositorioBase<Produto>, IRepositorioProduto
+    {
+        public IEnumerable<Produto> BuscarPorNome(string nome)
+        {
+            return contexto.Produtos.Where(p => p.Nome == nome);
+        }
+    }
+}
