91ae01d985304ac7381401b7c960905fcfe76345
Rodrigo Marcell <rodrigomarcell@outlook.com>
2021-10-22 11:48:04 -0400

    criando perfil automapper

---
+++ ProjetoModeloDDD.MVC/AutoMapper/PerfilMapeamento.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ProjetoModeloDDD.Dominio.Entidades;
+using ProjetoModeloDDD.MVC.ViewModels;
+
+namespace ProjetoModeloDDD.MVC.AutoMapper
+{
+    public class PerfilMapeamento : Profile
+    {
+        public PerfilMapeamento()
+        {
+            CreateMap<Cliente, ClienteViewModel>();
+            CreateMap<ClienteViewModel, Cliente>();
+        }
+    }
+}
