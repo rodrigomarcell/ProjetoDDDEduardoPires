566f2235fad24c2f6a41ca2c381d363915378a29
Rodrigo Marcell <rodrigomarcell@outlook.com>
2021-10-22 11:47:52 -0400

    adicionando referencia automapper e configurando serviço automapper

--- ProjetoModeloDDD.MVC/Startup.cs
+++ ProjetoModeloDDD.MVC/Startup.cs
@@ -24,6 +24,8 @@ namespace ProjetoModeloDDD.MVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
