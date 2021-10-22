3161fba015b644df00da632c923f7562f7daf990
Rodrigo Marcell <rodrigomarcell@outlook.com>
2021-10-22 11:49:04 -0400

    adicionando clientes controller

---
+++ ProjetoModeloDDD.MVC/Controllers/ClientesController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProjetoModeloDDD.Infra.Dados.Repositorios;
+using AutoMapper;
+using ProjetoModeloDDD.Dominio.Entidades;
+using ProjetoModeloDDD.MVC.ViewModels;
+
+namespace ProjetoModeloDDD.MVC.Controllers
+{
+    public class ClientesController : Controller
+    {
+        private readonly ClienteRepositorio clienteRepositorio = new ClienteRepositorio();
+        private readonly IMapper _mapper;
+
+        public ClientesController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        // GET: ClientesController
+        public ActionResult Index()
+        {
+            var clienteViewModel = _mapper.Map<IEnumerable<Cliente>, IEnumerable<ClienteViewModel>>(clienteRepositorio.ObterTodos());
+            return View(clienteViewModel);
+        }
+
+        // GET: ClientesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ClientesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ClientesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ClienteViewModel cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    
+                    var clienteDomain = _mapper.Map<ClienteViewModel, Cliente>(cliente);
+                    clienteRepositorio.Adicionar(clienteDomain);
+                    return RedirectToAction("Index");
+                }
+                catch
+                {
+                    return View(cliente);
+                }
+            }
+
+            return View(cliente);
+        }
+
+        // GET: ClientesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ClientesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ClientesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ClientesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
