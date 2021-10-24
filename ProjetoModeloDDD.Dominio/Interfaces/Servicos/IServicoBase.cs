71d5a7b4e50f58ec180bb94b4d0c74caa4b6ca82
Rodrigo Marcell <rodrigomarcell@outlook.com>
2021-10-24 19:50:49 -0400

    Criando interface de serviço

---
+++ ProjetoModeloDDD.Dominio/Interfaces/Servicos/IServicoBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoModeloDDD.Dominio.Interfaces.Servicos
+{
+    public interface IServicoBase<T> where T : class
+    {
+        void Adicionar(T entidade);
+
+        T ObterPorId(int id);
+
+        IEnumerable<T> ObterTodos();
+
+        void Atualizar(T entidade);
+
+        void Excluir(T entidade);
+
+        void Dispose();
+    }
+}
---
+++ ProjetoModeloDDD.Dominio/Interfaces/Servicos/IServicoProduto.cs
@@ -0,0 +1,13 @@
+﻿using ProjetoModeloDDD.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoModeloDDD.Dominio.Interfaces.Servicos
+{
+    public interface IServicoProduto : IServicoBase<Produto>
+    {
+    }
+}
