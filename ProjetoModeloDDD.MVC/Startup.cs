27d4e94506eed043e3a1df73b5f0d6b7182d8428
Rodrigo Marcell <rodrigomarcell@outlook.com>
2021-10-25 20:58:26 -0400

    injetando dependencia na camada de apresentacao pelo configureService

--- ProjetoModeloDDD.MVC/Startup.cs
+++ ProjetoModeloDDD.MVC/Startup.cs
@@ -1,13 +1,15 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ProjetoModeloDDD.Aplicacao;
+using ProjetoModeloDDD.Aplicacao.Interface;
+using ProjetoModeloDDD.Dominio.Interfaces.Repositorios;
+using ProjetoModeloDDD.Dominio.Interfaces.Servicos;
+using ProjetoModeloDDD.Dominio.Servicos;
+using ProjetoModeloDDD.Infra.Dados.Repositorios;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ProjetoModeloDDD.MVC
 {
@@ -21,9 +23,17 @@ namespace ProjetoModeloDDD.MVC
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public  void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews();  
+
+            services.AddScoped<IRepositorioCliente, ClienteRepositorio>();
+            services.AddScoped<IAppServicoCliente, AppServicoCliente>();
+            services.AddScoped<IServicoCliente, ServicoCliente>();
+
+            services.AddScoped<IRepositorioProduto, ProdutoRepositorio>();
+            services.AddScoped<IAppServicoProduto, AppServicoProduto>();
+            services.AddScoped<IServicoProduto, ServicoProduto>();
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         }
@@ -38,21 +48,18 @@ namespace ProjetoModeloDDD.MVC
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
+
             });
         }
     }
