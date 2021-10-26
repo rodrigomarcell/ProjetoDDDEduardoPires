e4f8d0e559d1f9d3e112af123ad3950d66ce5f68
Rodrigo Marcell <rodrigomarcell@outlook.com>
2021-10-25 20:59:13 -0400

    criando produtos controller

---
+++ ProjetoModeloDDD.MVC/Controllers/ProdutosController.cs
@@ -0,0 +1,103 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjetoModeloDDD.Aplicacao.Interface;
+using ProjetoModeloDDD.Dominio.Entidades;
+using ProjetoModeloDDD.MVC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoModeloDDD.MVC.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        private readonly IAppServicoProduto _produtoApp;
+        private readonly IAppServicoCliente _clienteApp;
+        private readonly IMapper _mapper;
+
+        public ProdutosController(IAppServicoProduto produtoApp, IAppServicoCliente clienteApp, IMapper mapper)
+        {
+            _produtoApp = produtoApp;
+            _clienteApp = clienteApp;
+            _mapper = mapper;
+        }
+
+        // GET: ProdutosController
+        public ActionResult Index()
+        {
+            var produtoViewModel = _mapper.Map<IEnumerable<Produto>, IEnumerable<ProdutoViewModel>>(_produtoApp.ObterTodos());
+            return View(produtoViewModel);
+        }
+
+        // GET: ProdutosController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProdutosController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProdutosController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProdutosController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProdutosController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProdutosController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProdutosController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
