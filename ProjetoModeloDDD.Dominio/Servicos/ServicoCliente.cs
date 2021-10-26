54dd698e6677031d5ddcefbd8d1cf08733efb3eb
Rodrigo Marcell <rodrigomarcell@outlook.com>
2021-10-25 21:01:24 -0400

    Implementando Servicos na camada de dominio

---
+++ ProjetoModeloDDD.Dominio/Servicos/ServicoCliente.cs
@@ -0,0 +1,26 @@
+﻿using ProjetoModeloDDD.Dominio.Entidades;
+using ProjetoModeloDDD.Dominio.Interfaces.Repositorios;
+using ProjetoModeloDDD.Dominio.Interfaces.Servicos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoModeloDDD.Dominio.Servicos
+{
+    public class ServicoCliente : ServicoBase<Cliente>, IServicoCliente
+    {
+        private readonly IRepositorioCliente _clienteRepositorio;
+
+        public ServicoCliente(IRepositorioCliente clienteRepositorio) : base(clienteRepositorio)
+        {
+            _clienteRepositorio = clienteRepositorio;
+        }
+
+        public IEnumerable<Cliente> ObterClientesEspeciais(IEnumerable<Cliente> clientes)
+        {
+            return clientes.Where(c => c.ClienteEspecial(c));
+        }
+    }
+}
---
+++ ProjetoModeloDDD.Dominio/Servicos/ServicoProduto.cs
@@ -0,0 +1,22 @@
+﻿using ProjetoModeloDDD.Dominio.Entidades;
+using ProjetoModeloDDD.Dominio.Interfaces.Repositorios;
+using ProjetoModeloDDD.Dominio.Interfaces.Servicos;
+using System;
+using System.Collections.Generic;
+
+namespace ProjetoModeloDDD.Dominio.Servicos
+{
+    public class ServicoProduto : ServicoBase<Produto>, IServicoProduto
+    {
+        private readonly IRepositorioProduto _produtoRepositorio;
+        public ServicoProduto(IRepositorioProduto produtoRepositorio) : base(produtoRepositorio)
+        {
+            _produtoRepositorio = produtoRepositorio;
+        }
+
+        public IEnumerable<Produto> BuscarPorNome(string nome)
+        {
+            return _produtoRepositorio.BuscarPorNome(nome);
+        }
+    }
+}
