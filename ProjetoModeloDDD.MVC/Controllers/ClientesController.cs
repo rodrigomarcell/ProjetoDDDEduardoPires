2814f16eec4d9089375b2c617246930f8c92f745
Rodrigo Marcell <rodrigomarcell@outlook.com>
2021-10-25 20:59:01 -0400

    finalizando implementacao controller cliente

--- ProjetoModeloDDD.MVC/Controllers/ClientesController.cs
+++ ProjetoModeloDDD.MVC/Controllers/ClientesController.cs
@@ -4,34 +4,47 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using ProjetoModeloDDD.Infra.Dados.Repositorios;
 using AutoMapper;
 using ProjetoModeloDDD.Dominio.Entidades;
 using ProjetoModeloDDD.MVC.ViewModels;
+using ProjetoModeloDDD.Aplicacao;
+using ProjetoModeloDDD.Dominio.Interfaces.Servicos;
+using ProjetoModeloDDD.Aplicacao.Interface;
 
 namespace ProjetoModeloDDD.MVC.Controllers
 {
     public class ClientesController : Controller
     {
-        private readonly ClienteRepositorio clienteRepositorio = new ClienteRepositorio();
+        
+        private readonly IAppServicoCliente _clienteApp;
         private readonly IMapper _mapper;
 
-        public ClientesController(IMapper mapper)
+        public ClientesController(IMapper mapper, IAppServicoCliente clienteApp)
         {
             _mapper = mapper;
+            _clienteApp = clienteApp;
         }
 
         // GET: ClientesController
         public ActionResult Index()
         {
-            var clienteViewModel = _mapper.Map<IEnumerable<Cliente>, IEnumerable<ClienteViewModel>>(clienteRepositorio.ObterTodos());
+            var clienteViewModel = _mapper.Map<IEnumerable<Cliente>, IEnumerable<ClienteViewModel>>(_clienteApp.ObterTodos());
+            return View(clienteViewModel);
+        }
+
+        public ActionResult Especiais()
+        {
+            var clienteViewModel = _mapper.Map<IEnumerable<Cliente>, IEnumerable<ClienteViewModel>>(_clienteApp.ObterClientesEspeciais());
             return View(clienteViewModel);
         }
 
         // GET: ClientesController/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            var cliente = _clienteApp.ObterPorId(id);
+            var clienteViewModel = _mapper.Map<Cliente, ClienteViewModel>(cliente); 
+
+            return View(clienteViewModel);
         }
 
         // GET: ClientesController/Create
@@ -51,7 +64,7 @@ namespace ProjetoModeloDDD.MVC.Controllers
                 {
                     
                     var clienteDomain = _mapper.Map<ClienteViewModel, Cliente>(cliente);
-                    clienteRepositorio.Adicionar(clienteDomain);
+                    _clienteApp.Adicionar(clienteDomain);
                     return RedirectToAction("Index");
                 }
                 catch
@@ -66,38 +79,46 @@ namespace ProjetoModeloDDD.MVC.Controllers
         // GET: ClientesController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            var cliente = _clienteApp.ObterPorId(id);
+            var clienteViewModel = _mapper.Map<Cliente, ClienteViewModel>(cliente);
+
+            return View(clienteViewModel);
         }
 
         // POST: ClientesController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, ClienteViewModel cliente)
         {
-            try
+            if (ModelState.IsValid)
             {
-                return RedirectToAction(nameof(Index));
+                var clienteDominio = _mapper.Map<ClienteViewModel, Cliente>(cliente);
+                _clienteApp.Atualizar(clienteDominio);
+
+                return RedirectToAction("Index");
             }
-            catch
-            {
-                return View();
-            }
+
+            return View();
         }
 
         // GET: ClientesController/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            var cliente = _clienteApp.ObterPorId(id);
+            var clienteViewModel = _mapper.Map<Cliente, ClienteViewModel>(cliente);
+            return View(clienteViewModel);
         }
 
         // POST: ClientesController/Delete/5
-        [HttpPost]
+        [HttpPost, ActionName("Delete")]      
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult DeleteConfirmado(int id)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                var cliente = _clienteApp.ObterPorId(id);
+                _clienteApp.Excluir(cliente);
+                return RedirectToAction("Index");
             }
             catch
             {
